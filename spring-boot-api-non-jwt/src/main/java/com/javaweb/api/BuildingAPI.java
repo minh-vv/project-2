a1e094f6799d3d9181d32d834d605afd45afd493
minh-vv <vuvanminh16082004@gmail.com>
2024-06-01 19:51:51 +0700

    update

--- spring-boot-api-non-jwt/src/main/java/com/javaweb/api/BuildingAPI.java
+++ spring-boot-api-non-jwt/src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,50 +1,25 @@
 package com.javaweb.api;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javaweb.beans.BuildingBean;
-import com.javaweb.beans.reponse.BuildingReponseDTO;
-import com.javaweb.customexception.InvalidDataException;
 import com.javaweb.service.BuildingService;
 
 @RestController
 public class BuildingAPI {
 	
-	@Autowired
-	private BuildingService buildingService;
-	
-		
 	@GetMapping(value = "/api/buildings")
-	private  Object getBuildings(@RequestParam (name="name", required=false) String name,
-								@RequestParam(name="numberOfBasement",required=false) Long numberOfBasement) {
+	private  Object getBuildings(@RequestParam (required=false) Map<String,String> params,
+								@RequestParam(name="typeCode",required=false) List<String> typeCode) {
 		// DB
-		List<BuildingReponseDTO> results = buildingService.findAll(name,numberOfBasement);
-		return results;
+		
+		return null;
 
 	}
 	
-	private void validate(BuildingBean buildBuildingDTO) {
-		if(buildBuildingDTO.getName() == null || buildBuildingDTO.getName().equals("") || buildBuildingDTO.getRentPrice() == null) {
-			throw new InvalidDataException("name or distritctId la null");
-		}
-	}
-	
-	@PostMapping(value = "/api/buildings")
-	private Object createBuildings(@RequestBody BuildingBean building) {
-		validate(building);
-		return building;
-	}
-	@DeleteMapping(value ="/api/buildings/{ids}")
-	private void deleteBuildings(@PathVariable List<Long> ids) {
-		System.out.print("id")	;
-	}
 }
